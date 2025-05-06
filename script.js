1e5085fa708c53a322b9586d85242cf10c6dccf6
EdLaps1w3r <edlaps1w3r@gmail.com>
2025-05-06 15:41:49 +0800

    Added UI to rps
    Altered code to work with new UI elements.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+humanScore = 0;
+computerScore = 0;
+
 function getRandomInt(max) {
     return Math.floor(Math.random() * max);
 }
@@ -13,74 +16,35 @@ function getComputerChoice() {
     }
 }
 
-function getHumanChoice(currentRound){
-    let choice = "";
-    let inputIsValid = false;
-    let validChoices = ["rock", "paper", "scissors"];
-    let cRoundCorrected = currentRound + 1;
-    while (inputIsValid == false) {
-        choice = prompt("Round: " + cRoundCorrected + ". Please enter your choice of move. ", "(rock, paper or scissors)").toLowerCase().trim();
-        if (validChoices.includes(choice) == true) {
-            inputIsValid = true;
-        } else {
-            alert("Ivalid Input, please input one of the given choices");
-        }
-    }
-    return choice;
-}
-
-function playRound(humanChoice, computerChoice, humanWonRound) {
+function playRound(humanChoice, computerChoice) {
     let humanWins = ["rockscissors", "paperrock"];
     let combinedChoice = humanChoice + computerChoice;
     if (humanChoice == computerChoice) {
-        alert("Both selected " + humanChoice + ", the round was a draw, let's replay it.");
-        return "draw";
+        alert("Both selected " + humanChoice + ", the round was a draw.");
     } else if (humanWins.includes(combinedChoice) == true) {
         alert("You selected " + humanChoice + " and the computer selected " + computerChoice + ", the round was your win.");
-        return "true";
+        document.getElementById("humanScore").innerHTML = "Your Score:" + (humanScore + 1);
+        humanScore++;
+        if (humanScore == 5) {
+            document.getElementById("winMessage").innerHTML = "Congrats you Win! Refresh the page to play again."
+            const buttons = document.getElementById("buttons");
+            buttons.removeChild(buttons.firstElementChild);
+            buttons.removeChild(buttons.firstElementChild);
+            buttons.removeChild(buttons.firstElementChild);
+        }
+        return humanScore;
     } else {
         alert("You selected " + humanChoice + " and the computer selected " + computerChoice + ", the round was your loss.");
-        return "false";
+        document.getElementById("computerScore").innerHTML = "Computer Score:" + (computerScore + 1);
+        computerScore++
+        if (computerScore == 5) {
+            document.getElementById("winMessage").innerHTML = "Better luck next time you lost. Refresh the page to play again."
+            const buttons = document.getElementById("buttons");
+            buttons.removeChild(buttons.firstElementChild);
+            buttons.removeChild(buttons.firstElementChild);
+            buttons.removeChild(buttons.firstElementChild);
+        }
+        return computerScore;
     }
 
 }
-
-function playGame(rounds) {
-    alert("Let's play a game of rock paper scissors!")
-    let humanScore = 0;
-    let computerScore = 0;
-    let humanWonRound = "draw";
-    for (let i = 0; i < rounds; i++) {
-        let notDraw = false;
-        while  (notDraw == false) {
-            const humanSelection = getHumanChoice(i);
-            const computerSelection = getComputerChoice();
-            humanWonRound = playRound(humanSelection, computerSelection, humanWonRound);
-            if (humanWonRound != "draw") {
-                notDraw = true;
-            }
-        }
-        if (humanWonRound == "true") {
-            humanScore++;
-        } else {
-            computerScore++;
-        }
-        alert("The new score is: You: " + humanScore + " Computer: " + computerScore);
-        if (humanScore == 3 || computerScore == 3) {
-            break;
-        }
-    }
-    if (humanScore > computerScore) {
-        alert("Congrats you won! The final score was You: " + humanScore + " and Computer: " + computerScore);
-        return "Human Won";
-    } else {
-        alert("Better luck next time, you lost. The score was You: " + humanScore + " and Computer: " + computerScore)
-        return "Computer Won"
-    }
-}
-
-
-console.log(playGame(5));
-
-
-
